c553d239d32e0d88fd99f126116f7f1c9696c2b6
Anshuman Chhabra <anshuman.lalakers@gmail.com>
2021-10-05 22:34:43 -0700

    Further experiments with ideo data

--- Noyce/load_data.py
+++ Noyce/load_data.py
@@ -9,6 +9,36 @@ REDDIT_COMMENTS = "./UCD_Noyce/Noyce/data/ideology/reddit_comments_onesided.csv"
 REDDIT_COMMENTS_POL = "./UCD_Noyce/Noyce/data/ideology/reddit_political_comments_85.csv"
 
 
+def load_ideo_addn_slant1():
+
+    df = pd.read_csv(
+        "./UCD_Noyce/Noyce/data/IDEOLOGICAL/additionals/train_slant.csv", encoding='unicode_escape')
+    df_test = pd.read_csv(
+        "./UCD_Noyce/Noyce/data/IDEOLOGICAL/additionals/test_old.csv", encoding='unicode_escape')
+    
+    df = df.dropna()
+    df_test = df_test.dropna()
+    
+    df['text'] = df['text'].apply(normalize)
+    df_test['text'] = df_test['text'].apply(normalize)
+    return df['text'].tolist(), df['class_id'].astype(int).tolist(), df_test['text'].tolist(), df_test['class_id'].astype(int).tolist()
+
+
+def load_ideo_addn_slant2():
+
+    df = pd.read_csv(
+        "./UCD_Noyce/Noyce/data/IDEOLOGICAL/additionals/train_new_slant.csv", encoding='unicode_escape')
+    df_test = pd.read_csv(
+        "./UCD_Noyce/Noyce/data/IDEOLOGICAL/additionals/test_old.csv", encoding='unicode_escape')
+    
+    df = df.dropna()
+    df_test = df_test.dropna()
+    
+    df['text'] = df['text'].apply(normalize)
+    df_test['text'] = df_test['text'].apply(normalize)
+    return df['text'].tolist(), df['class_id'].astype(int).tolist(), df_test['text'].tolist(), df_test['class_id'].astype(int).tolist()
+
+
 def load_ideo_addn_newold():
 
     df = pd.read_csv(
@@ -264,6 +294,10 @@ def load_disagreement_data():
 
 
 def load_data(dset_name='political_final', path = '', test_set = True):
+    if dset_name == 'ideology_addn_newslant':
+        return load_ideo_addn_slant2()
+    if dset_name == 'ideology_addn_slant':
+        return load_ideo_addn_slant1()
     if dset_name == 'ideology_addn_newold':
         return load_ideo_addn_newold()
     if dset_name == 'ideology_final':
