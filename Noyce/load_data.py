fd91cc9bfbc717b2946585c606f5a20b088f61ce
Anshuman Chhabra <anshuman.lalakers@gmail.com>
2021-10-25 17:34:01 -0700

    Updated load data file

--- Noyce/load_data.py
+++ Noyce/load_data.py
@@ -8,6 +8,20 @@ YOUTUBE_POSTS = "./UCD_Noyce/Noyce/data/ideology/youtube.csv"
 REDDIT_COMMENTS = "./UCD_Noyce/Noyce/data/ideology/reddit_comments_onesided.csv"
 REDDIT_COMMENTS_POL = "./UCD_Noyce/Noyce/data/ideology/reddit_political_comments_85.csv"
 
+def load_ideo_FIN():
+
+    df = pd.read_csv(
+        "./UCD_Noyce/Noyce/data/IDEOLOGICAL/additionals/train_FIN.csv", encoding='unicode_escape')
+    df_test = pd.read_csv(
+        "./UCD_Noyce/Noyce/data/IDEOLOGICAL/additionals/test_FIN.csv", encoding='unicode_escape')
+    
+    df = df.dropna()
+    df_test = df_test.dropna()
+    
+    df['text'] = df['text'].apply(normalize)
+    df_test['text'] = df_test['text'].apply(normalize)
+    return df['text'].tolist(), df['class_id'].astype(int).tolist(), df_test['text'].tolist(), df_test['class_id'].astype(int).tolist()
+
 def load_ideo_addn_yt_slant_pol2():
 
     df = pd.read_csv(
@@ -400,6 +414,8 @@ def load_disagreement_data():
 
 
 def load_data(dset_name='political_final', path = '', test_set = True):
+    if dset_name == 'ideology_FIN':
+        return load_ideo_FIN()
     if dset_name == 'ideology_addn_yt_slant_pol2':
         return load_ideo_addn_yt_slant_pol2()
     if dset_name == 'ideology_addn_yt_slant_pol':
