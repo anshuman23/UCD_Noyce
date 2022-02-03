d778e5e7f33fc439061c92bbd0c23657fe3889c3
Anshuman <anshuman.lalakers@gmail.com>
2022-02-03 12:34:48 -0800

    Another exp for ideo

--- Noyce/load_data.py
+++ Noyce/load_data.py
@@ -8,6 +8,20 @@ YOUTUBE_POSTS = "./UCD_Noyce/Noyce/data/ideology/youtube.csv"
 REDDIT_COMMENTS = "./UCD_Noyce/Noyce/data/ideology/reddit_comments_onesided.csv"
 REDDIT_COMMENTS_POL = "./UCD_Noyce/Noyce/data/ideology/reddit_political_comments_85.csv"
 
+def load_ideo_article_combined():
+
+    df = pd.read_csv(
+        "./UCD_Noyce/Noyce/data/train_combined.csv", encoding='unicode_escape')
+    df_test = pd.read_csv(
+        "./UCD_Noyce/Noyce/data/test_combined.csv", encoding='unicode_escape')
+    
+    df = df.dropna()
+    df_test = df_test.dropna()
+    
+    df['text'] = df['text'].apply(normalize)
+    df_test['text'] = df_test['text'].apply(normalize)
+    return df['text'].tolist(), df['class_id'].astype(int).tolist(), df_test['text'].tolist(), df_test['class_id'].astype(int).tolist()
+
 
 def load_ideo_article():
 
@@ -446,6 +460,8 @@ def load_disagreement_data():
 
 
 def load_data(dset_name='political_final', path = '', test_set = True):
+    if dset_name == 'ideology_article_combined':
+        return load_ideo_article_combined()
     if dset_name == 'ideology_article':
         return load_ideo_article()
     if dset_name == 'filterer':
